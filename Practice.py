7eccc9b05a172021d94f49296b151d7e25966dce
rajrajputrs <rajrajputrs575@gmail.com>
2026-02-09 12:02:57 +0530

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,13 @@
+#class Solution:
+   # def getSum(self, a: int, b: int) -> int:
+    #    a,b=1,2
+   #     lst=[]
+   #     while a!=0:
+   #         lst.append("1")
+   ##         a-=1
+    #    while b!=0:
+   #         lst.append("1")
+    #        b-=1
+    #    print(lst.count())
+lst=[1,2,3]
+print(lst.count())
