e6e759cf496ff648bc7a4fcdb347a53f2f81f910
RAJ RAJPUT <rajrajputrs575@gmail.com>
2026-07-08 18:46:31 +0530

    Delete Projects directory

--- Projects/data_cleaning.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-df = pd.read_csv(r"C:\Users\rajra\OneDrive\Desktop\global_gdp_inflation_2000_2024.csv")
-
-print(df.head())
-print(df.info())
-print(df.describe())
