2245b6c9abfda7299fcd4218ab4b5134cf166193
RAJ RAJPUT <rajrajputrs575@gmail.com>
2026-07-08 18:46:59 +0530

    Delete Math directory

--- Math/data_cleaning.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as mlt
-
-df = pd.read_csv(r"C:\Users\rajra\OneDrive\Desktop\Batsman_Data.csv")
-
-mlt.scatter(df["Match_ID"],df["Runs"])
-mlt.show()
